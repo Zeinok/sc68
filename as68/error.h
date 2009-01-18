9ced8c1072f3acc12859f34c0aa4bf043b844f1d
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2009-01-18 12:47:24 +0000

    as68 - mc68000 macro asssembler: import.

---
+++ as68/error.h
@@ -0,0 +1,30 @@
+/*
+ *		     as68 - 68000 macro assembler
+ *		   Copyright (C) 1993 Vincent Penne
+ *	       Copyright (C) 1999-2009 Benjamin Gerard
+ *
+ * This  program is  free  software: you  can  redistribute it  and/or
+ * modify  it under the  terms of  the GNU  General Public  License as
+ * published by the Free Software  Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+ * General Public License for more details.
+ *
+ * You should have  received a copy of the GNU  General Public License
+ * along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*$Id$*/
+
+#ifndef _AS68_ERROR_H_
+#define _AS68_ERROR_H_
+
+extern char *error_list[];
+extern char *warn_list[];
+
+#endif
