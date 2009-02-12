dd4972009028ce85c75cde272cd6ead7c694de3a
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2009-02-12 00:01:20 +0000

    import sc68-doc; (mostly developer) generated documentations

---
+++ sc68-doc/sc68_doc.h
@@ -0,0 +1,70 @@
+/* This file exists for documentation purpose only. */
+/**
+@defgroup  cli_tools68  sc68 command line tools
+
+  Documents all sc68 related command line tools. 
+
+*/
+
+/**
+@defgroup  devtools68  sc68 developer tools
+@ingroup   cli_tools68
+
+  Documents all sc68 related command line tools useful for
+  developers.
+
+*/
+
+/**
+@defgroup  usertools68  sc68 user tools
+@ingroup   cli_tools68
+
+  Documents all sc68 related command line tools useful for
+  users.
+
+*/
+
+/**
+@defgroup  api68 sc68 APIs
+
+  Documents sc68 and buddies libraries.
+
+  This documention is for developers that want to use sc68 or its
+  component in their own programs.
+
+  The @ref sc68_lib is the higher level component. It depends on @ref
+  file68_lib which main purpose is to handle sc68 resources (player,
+  files, config ...). @ref unice68_lib is optionnaly used for depacking
+  ICE! file a common compressor format in the Atari ST world. Finally
+  @ref desa68_lib is used by extra programs such as @ref sourcer68_cli
+  or debug68_cli for disassemble M68k machine code.
+  
+
+*/
+
+
+/**
+@defgroup file68_lib file68 library
+@ingroup  api68
+
+  How to use file68 access sc68 files and resources library in your
+  programs.
+
+*/
+
+/**
+@defgroup sc68_lib sc68 library
+@ingroup  api68
+
+  How to use sc68 Atari ST and Amiga music emulator library in your
+  programs.
+
+*/
+
+
+/**
+   @mainpage sc68 documentation
+
+   This is sc68 documentation.
+
+ */
