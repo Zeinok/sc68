333362d07982cc77eefa0396bf07656ee536872f
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2010-03-09 03:40:28 +0000

    Add id keyword for m4 files.

--- tools/svn-setprops.sh
+++ tools/svn-setprops.sh
@@ -5,15 +5,16 @@
 svn info >/dev/null || exit 255
 
 # Files eligible for 'Id' keyword property
-find . -type f '(' \
- -name '*.[ch]' -o\
- -name '*.in' -o\
- -name '*.sh' -o\
- -name 'Makefile.am' -o\
- -name 'configure.ac'\
- ')' -execdir svn propset svn:keywords Id {} ';'
+find . -type f '('  \
+    -name '*.[ch]' -o \
+    -name '*.in' -o \
+    -name '*.m4' -o \
+    -name '*.sh' -o \
+    -name 'Makefile.am' -o \
+    -name 'configure.ac' \
+    ')' -execdir svn propset svn:keywords Id {} ';'
 
 # Files for execute 
 find . -type f '(' \
- -name '*.sh' \
- ')' -execdir svn propset svn:executable '*' {} ';'
+    -name '*.sh' \
+    ')' -execdir svn propset svn:executable '*' {} ';'
