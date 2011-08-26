e2334420d6b70b9d4fe40d878223429bd20c71ba
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2011-08-26 23:36:34 +0000

    Add missing files.

---
+++ mksc68/mksc68_doc.h
@@ -0,0 +1,25 @@
+/* This file exists for documentation purpose only. */
+/**
+@defgroup  debug68_prg  debug68 program
+@ingroup   devtools68
+
+  Create and debug sc68 files.
+
+  @section author AUTHOR
+
+    Copyright (C) 1998-2009 Benjamin Gerard
+
+    This program is free software.
+
+  @section date DATE
+
+    2003
+
+  @section synopsis SYNOPSIS
+
+    debug68 [options ... ] <file>
+
+  @section options OPTIONS
+
+    @arg @b --help                    Display this message and exit.
+*/
