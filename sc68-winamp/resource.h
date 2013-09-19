dc967698e989f1985a07131bf993601e0873cc6a
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2013-09-19 09:36:32 +0000

    Add config dialog to resource.

--- sc68-winamp/resource.h
+++ sc68-winamp/resource.h
@@ -3,6 +3,7 @@
 #endif
 
 #define IDD_DLG_FILEINFO                        102
+#define IDD_DLG_CONFIG                          104
 #define IDC_INF_URI                             1003
 #define IDC_INF_FORMAT                          1005
 #define IDC_INF_GENRE                           1009
@@ -18,3 +19,9 @@
 #define IDC_INF_AMIGA                           1056
 #define IDC_INF_YM                              1057
 #define IDC_INF_TRACK                           1058
+#define IDC_INF_YMENGINE                        1061
+#define IDC_INF_YMFILTER                        1069
+#define IDC_INF_YMVOLUME                        1070
+#define IDC_INF_AGABLEND                        1071
+#define IDC_INF_AGAFILTER                       1072
+#define IDC_INF_SPR                             1074
