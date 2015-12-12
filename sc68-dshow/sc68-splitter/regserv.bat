3b50c997fb4bac6a431ff48a956ced175a39eb5b
benjihan <benjihan@5a031502-5ecf-44a1-884c-8ae1cb1c7d14>
2015-12-12 20:40:29 +0000

    Bit of maintainance around sc68-dshow project.

--- sc68-dshow/sc68-splitter/regserv.bat
+++ sc68-dshow/sc68-splitter/regserv.bat
@@ -1,7 +1,8 @@
 @REM Register COM server
-@echo OFF
 echo PWD=%cd%
 echo Register server %1
 del %2
-runas /Administrators regsvr32 /s /u "%1" && echo Unregistred
-runas /Administrators regsvr32 /s    "%1" && echo Registered && echo OK>%2
+@REM runas /Administrators 
+regsvr32 /s /u "%1" && echo Unregistred
+@REM runas /Administrators
+regsvr32 /s    "%1" && echo Registered && echo OK>%2
